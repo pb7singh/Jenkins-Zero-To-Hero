fe9f2e26711fc999b40b9dbafce946c67a52498c
pb7singh <85266985+pb7singh@users.noreply.github.com>
2024-11-06 18:05:46 -0500

    reverted_back_to_oroginal

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -10,31 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StartApplication {
 
-    // Duplicate Code Example
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfully built a Spring Boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on Kubernetes using Argo CD");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
-    }
-
-    // Another endpoint with duplicate code
-    @GetMapping("/another")
-    public String another(final Model model) {
-        model.addAttribute("title", "I have successfully built a Spring Boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on Kubernetes using Argo CD");
-        return "index";
-    }
-
-    // Hardcoded Sensitive Data (for security issues detection)
-    public void someMethod() {
-        String password = "admin123";  // Avoid hardcoding sensitive information
-        System.out.println("Password: " + password);
-    }
-
-    // Another method to demonstrate a potential lack of test coverage
-    public void untestedMethod() {
-        System.out.println("This method is not tested!");
     }
 
     public static void main(String[] args) {
